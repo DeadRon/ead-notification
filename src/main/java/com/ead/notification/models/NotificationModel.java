6dcd9afa91c1ab90f39aaf6e19397f0df551c5d0
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-03-01 01:18:59 -0300

    [SEMANA 6] Preparar Asynchronous Communication via Commands em Notification Microservice

---
+++ src/main/java/com/ead/notification/models/NotificationModel.java
@@ -0,0 +1,81 @@
+package com.ead.notification.models;
+
+import com.ead.notification.enums.NotificationStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_NOTIFICATIONS")
+public class NotificationModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID noticationId;
+    @Column(nullable = false)
+    private UUID userId;
+    @Column(nullable = false, length = 150)
+    private String title;
+    @Column(nullable = false)
+    private String message;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime criationDate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus notificationStatus;
+
+    public UUID getNoticationId() {
+        return noticationId;
+    }
+
+    public void setNoticationId(UUID noticationId) {
+        this.noticationId = noticationId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCriationDate() {
+        return criationDate;
+    }
+
+    public void setCriationDate(LocalDateTime criationDate) {
+        this.criationDate = criationDate;
+    }
+
+    public NotificationStatus getNotificationStatus() {
+        return notificationStatus;
+    }
+
+    public void setNotificationStatus(NotificationStatus notificationStatus) {
+        this.notificationStatus = notificationStatus;
+    }
+}
